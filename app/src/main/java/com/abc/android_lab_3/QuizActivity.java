55efdd96b4ee4846821020ada53247491a0d4e05
luiscarlold <luiscarlold@outlook.com>
2020-03-18 20:11:25 -0600

    new changes

--- app/src/main/java/com/abc/android_lab_3/QuizActivity.java
+++ app/src/main/java/com/abc/android_lab_3/QuizActivity.java
@@ -1,5 +1,6 @@
 package com.abc.android_lab_3;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -17,37 +18,46 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     public void clickDisplayCorrectToast(View view) {
-        Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+
+        Intent intent = new Intent(this, ActivityTest.class);
+        startActivity(intent);
     }
 
     public void clickDisplayIncorrectToast(View view) {
         Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
     }
 
+
+    public void onStart(String s) {
+        super.onStart();
+        Log.d(TAG, "********onStart() called********");
+    }
+
     @Override
     public void onStart() {
         super.onStart();
-        Log.d(TAG, "onStart() called");
+        Log.d(TAG, "********onStart() called********");
     }
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG, "onResume() called");
+        Log.d(TAG, "********onResume() called********");
     }
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG, "onPause() called");
+        Log.d(TAG, "********onPause() called********");
     }
     @Override
     public void onStop() {
         super.onStop();
-        Log.d(TAG, "onStop() called");
+        Log.d(TAG, "********onStop() called********");
     }
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG, "onDestroy() called");
+        Log.d(TAG, "********onDestroy() called********");
     }
 
 
